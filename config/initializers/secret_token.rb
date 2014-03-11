b0bd66a69a4ea3eba22d907231a92f6136efb3a1
Colin Frankish <colin.frankish@gmail.com>
2014-03-11 18:46:20 +0000

    First commit, empty rails app, rspec/cucumber installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPractise::Application.config.secret_key_base = '784a48f5a4ebb22077d8ad3c3420c9004d9ac8e2d5356216ba11f7d9830117ad835e5ac7ec98dcc90b751e13bd05937bf4c5943433df820f3d4342569708f66a'
